e5545a507cfa32831ee9f45c01890247ccd9902a
Sydney <29594993+Form-And-Function@users.noreply.github.com>
2024-06-07 19:43:08 -0700

    fix resend auth

--- src/auth.ts
+++ src/auth.ts
@@ -3,10 +3,14 @@ import Google from "next-auth/providers/google";
 import { SupabaseAdapter } from "@auth/supabase-adapter"
 import Resend from "next-auth/providers/resend"
 
+console.log(process.env.AUTH_RESEND_API_KEY);
 
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google, Resend],
+  providers: [Google, Resend({
+    from: "match@reciprocal.love",
+    apiKey: process.env.AUTH_RESEND_API_KEY ?? "",
+  })],
   adapter: SupabaseAdapter({
     url: process.env.SUPABASE_URL ??  "",
     secret: process.env.SUPABASE_SERVICE_ROLE_KEY ?? "",
