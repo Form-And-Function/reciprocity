22accd7b9bb513b15b9f57ee0442d3c39427dc5e
Sydney <29594993+Form-And-Function@users.noreply.github.com>
2024-06-07 23:30:00 -0700

    instructions

--- src/app/loggedin.tsx
+++ src/app/loggedin.tsx
@@ -9,16 +9,50 @@ export default async function LoggedIn() {
         return null;
     }
     return (
-        <div>
+        <div className="instructions">
             <SignOut />
+
+            <p>
+                Welcome to reciprocal.love! Please fill out your profile below.
+
+                <br /> Write whatever you want. But please follow our community principles:
+            </p>
+
+            <br />
+            <ul>
+                <li>Be kind and respectful</li>
+                <li>Be honest</li>
+                <li>Try to make the platform better for everyone</li>
+            </ul>
+
+            <br />
+            <p>
+                Not sure what to write? Here are some ideas:
+            </p>
+
+            
+            <ul>
+                <li>What are you looking for?</li>
+                <li>What are you like?</li>
+                <li>How often are you on here? Should people still ask you out if you did not check them?</li>
+                <li>Get a friend to write a blurb on yourself.</li>
+            </ul>
+            <p>
+                But you could also put a poem, links to your social media, or a tirade on why en passant should be banned in chess. Everything is up to you!
+            </p>
+
+            <br />
             <h2>Your profile</h2>
+
+            <br />
+
             <ProfileEditor
-                // displayName="John Doe"
-                // bio=""
-                // age={20}
-                // pictures={[]}
-                // email={session.user.email}
-                />
+            // displayName="John Doe"
+            // bio=""
+            // age={20}
+            // pictures={[]}
+            // email={session.user.email}
+            />
             <br />
 
             <br />
@@ -26,4 +60,4 @@ export default async function LoggedIn() {
 
         </div>
     )
-    }
+}
